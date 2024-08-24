fb0e54b4ec86673eea086c88ce3e4741ae8834af
youssef <youssefbenyamine2eme@gmail.com>
2024-08-24 17:43:33 +0300

    test commit

---
+++ application.c
@@ -0,0 +1,17 @@
+#include "application.h"
+
+std_ReturneType ret = E_NOT_OK;
+
+
+//servo_motor_t servo;
+
+int main(void)
+{ 
+    ecu_layer_initialize();
+
+    while(loop)
+    {    
+
+    }   
+    return (EXIT_SUCCESS);
+}
